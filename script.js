acd44d62b890897a7a724b075d7381d36dd06659
Dariusz Okonski <darek200180@gmail.com>
2021-02-26 21:49:30 +0000

    add js to project

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello world')
+const boxes = document.querySelectorAll('.box')
+
+window.onload = setTimeout(() => {
+    checkBoxes()
+
+}, 1500);
+
+window.addEventListener('scroll', checkBoxes)
+
+function checkBoxes() {
+    const triggerBottom = (window.innerHeight / 5 * 4)
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerBottom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
+}
